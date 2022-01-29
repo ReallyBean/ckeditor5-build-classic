488faf2e05097fcc7ec7fdda1ade42b4108ff33d
ReallyBean <top_bean@outlook.com>
2022-01-29 11:02:41 +0900

    FontColor, FontBackgroundColor追加

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -29,6 +29,8 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor';
+import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -56,7 +58,9 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+	FontColor,
+	FontBackgroundColor
 ];
 
 // Editor configuration.
@@ -73,6 +77,9 @@ ClassicEditor.defaultConfig = {
 			'|',
 			'indent',
 			'outdent',
+			'|',
+			'fontColor',
+			'fontBackgroundColor',
 			'|',
 			'imageUpload',
 			'blockQuote',
